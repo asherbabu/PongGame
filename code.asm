f114cf2d92628d866ffed02b66e37d04063cf7ad
asherbabu <88152482+asherbabu@users.noreply.github.com>
2024-06-14 12:34:40 +0530

    pixel created

--- code.asm
+++ code.asm
@@ -15,9 +15,16 @@ CODE SEGMENT PARA 'CODE'
 
         MOV AH, 0bh   ; set the configuration to the background color
         MOV BH, 00h   ; to the background color
-        MOV BL, 00h   ; choosing background color
+        MOV BL, 01h   ; choosing background color
         INT 10h       ; executing the configuration
-        
+
+        MOV AH, 0Ch   ; set the configuration to write a pixel
+        MOV AL, 0Fh   ; choose color
+        MOV BH, 00h   ; set the page number 
+        MOV CX, 0Ah   ; set the column (X)
+        MOV DX, 0Ah   ; set the line (Y)
+        INT 10h       ; execute configuration
+
         RET           ; Return from procedure
     MAIN ENDP
 
