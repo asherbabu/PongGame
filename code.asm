79305f0fde6ee39143c546a1459f93af003317e2
asherbabu <88152482+asherbabu@users.noreply.github.com>
2024-06-14 12:22:44 +0530

    background  color added

--- code.asm
+++ code.asm
@@ -12,6 +12,11 @@ CODE SEGMENT PARA 'CODE'
         MOV AH, 00h   ; set the configuration to video mode
         MOV AL, 13h   ; choose videomode
         INT 10h       ; execute the configuration
+
+        MOV AH, 0bh   ; set the configuration to the background color
+        MOV BH, 00h   ; to the background color
+        MOV BL, 00h   ; choosing background color
+        INT 10h       ; executing the configuration
         
         RET           ; Return from procedure
     MAIN ENDP
